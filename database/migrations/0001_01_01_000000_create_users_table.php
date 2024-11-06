b729d989bcaaba95467af6946041a6265864ccb2
Muhammad Alfarozi <mr.alfarozy.a.n@gmail.com>
2024-11-06 23:15:34 +0700

    Update database migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,47 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            // $table->uuid('id')->primary();
-            // $table->string('name');
-            // $table->string('email')->unique();
-            // $table->timestamp('email_verified_at')->nullable();
-            // $table->string('google_id')->nullable();
-            // $table->string('password')->nullable();
-            // $table->string('status')->default('unverified');
-            // $table->rememberToken();
-            // $table->timestamps();
-            $table->uuid('id_user')->primary();
-            $table->string('nama');
-            $table->string('email')->unique();
-            $table->string('no_telp')->nullable();
-            $table->date('tgl_lahir')->nullable();
-            $table->string('alamat')->nullable();
-            $table->string('nik')->unique()->nullable();
-            $table->string('no_rek')->nullable();
-            $table->string('ktp_url')->nullable();
-            $table->string('kk_url')->nullable();
-            $table->string('foto')->nullable();
-            $table->integer('total_point')->default(0);
-            $table->decimal('berat_sampah', 8, 2)->default(0);
-            $table->string('roles');
-            $table->timestamps();
-        });
-
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
         Schema::create('soft_token_lifetimes', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('token');
@@ -75,9 +40,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
         Schema::dropIfExists('personal_access_tokens');
         Schema::dropIfExists('soft_token_lifetimes');
     }
