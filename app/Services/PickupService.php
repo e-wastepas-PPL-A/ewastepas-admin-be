928fb5cb11c68d1a86e58b3ea9c3ca2dcf004d37
Muhammad Alfarozi <mr.alfarozy.a.n@gmail.com>
2025-01-19 13:48:16 +0700

    update pickup id

--- app/Services/PickupService.php
+++ app/Services/PickupService.php
@@ -242,7 +242,7 @@ class PickupService
             return [false, 'Data tidak ditemukan', []];
         }
         $pickupDetails = PickupDetail::query()
-            ->where('pickup_id', $data->id)
+            ->where('pickup_id', $data->pickup_id)
             ->when($search, function ($query) use ($search, $id) {
                 $query->where("quantity", 'LIKE', "%$search%")->orWhere('pickup_id', $id); // Search for community name
             })
